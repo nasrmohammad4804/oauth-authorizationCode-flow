bf82133c92b90f169c471d6600752dbad5909ce0
mohammad nasr <nasrmohammad4804@gmail.com>
2022-03-13 00:28:54 +0330

    add client for authorization code flow

---
+++ authorization-code-flow/client/src/main/java/com/nasr/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.nasr.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
---
+++ authorization-code-flow/client/src/main/java/com/nasr/client/domain/Employee.java
@@ -0,0 +1,17 @@
+package com.nasr.client.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String empId;
+
+    private String name;
+}
